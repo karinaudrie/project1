d3cf5ec45b4d3d85ff7ae9c242cca1538538170f
karinaudrie <98987059+karinaudrie@users.noreply.github.com>
2022-03-25 00:41:18 -0700

    deploy fix error 15

--- project1/settings.py
+++ project1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nws!m-4-(i9k!xblf=9u1v)o8lf2-^17kny)!hu4#b31v_o()=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cmpt2367-torontocupcake.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['cmpt2367-torontocupcake.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
