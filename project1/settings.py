9dfa24c72435135659ace8a81e404f68ad1795e1
karinaudrie <98987059+karinaudrie@users.noreply.github.com>
2022-03-25 00:19:31 -0700

    deploy fix error 12

--- project1/settings.py
+++ project1/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
