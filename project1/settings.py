fd10fa8ccfd81f35be03714e42bf593061ef85d1
karinaudrie <98987059+karinaudrie@users.noreply.github.com>
2022-03-25 01:05:13 -0700

    deploy fix error 18

--- project1/settings.py
+++ project1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nws!m-4-(i9k!xblf=9u1v)o8lf2-^17kny)!hu4#b31v_o()='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cmpt2367-torontocupcake.herokuapp.com', '127.0.0.1']
 
