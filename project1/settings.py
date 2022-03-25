d4575521903d59c2d37a0b8c894b93da43bd1de2
karinaudrie <98987059+karinaudrie@users.noreply.github.com>
2022-03-24 23:49:09 -0700

    deploy fix error 6

--- project1/settings.py
+++ project1/settings.py
@@ -130,6 +130,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
